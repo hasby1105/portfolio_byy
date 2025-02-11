0d933ccfe7afefda11d55043758a6868b42b0ebb
Hasby Aee <hasbynasution4@gmail.com>
2025-02-11 10:34:54 +0700

    commit pertama

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: AboutComponent },  
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'contact', component: HomeComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
